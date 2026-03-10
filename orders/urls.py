9c32fd406d081fc7512f9afd25d2458719e7e255
AkireL <iamdleonor@gmail.com>
2026-03-10 14:37:02 -0600

    remove word api for urls

--- orders/urls.py
+++ orders/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('processed/<uuid:token>/', OrderProcessedView.as_view(), name='order-processed'),
     path('cart/update-item/<int:pk>/', update_order_item, name='cart-update-item'),
     path('cart/remove-item/<int:pk>/', remove_order_item, name='cart-remove-item'),
-    path('api/order/<int:pk>/', OrderDetailView.as_view(), name='api-order-detail'),
-    path('api/order/<int:pk>/mark-paid/', OrderMarkPaidView.as_view(), name='api-order-mark-paid'),
+    path('<int:pk>/', OrderDetailView.as_view(), name='api-order-detail'),
+    path('<int:pk>/mark-paid/', OrderMarkPaidView.as_view(), name='api-order-mark-paid'),
 ]
--- payments/urls.py
+++ payments/urls.py
@@ -9,6 +9,6 @@ app_name = "payments"
 urlpatterns = [
     path("create-session/", create_session_view, name="create-session"),
     path("checkout/<uuid:token>/", PaymentCheckoutView.as_view(), name="checkout"),
-    path("api/sessions/<uuid:token>/", PaymentSessionDetailView.as_view(), name='api-session-detail'),
-    path("api/sessions/invalidate/", InvalidateSessionsView.as_view(), name='api-invalidate-sessions'),
+    path("sessions/<uuid:token>/", PaymentSessionDetailView.as_view(), name='api-session-detail'),
+    path("sessions/invalidate/", InvalidateSessionsView.as_view(), name='api-invalidate-sessions'),
 ]
