f3a75b43233f2cd248e64b19535d79a39f47a623
AkireL <iamdleonor@gmail.com>
2026-03-11 22:05:32 -0600

    added groups and added a new logo

--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+def is_admin_or_editor(self):
+    return self.groups.filter(name__in=['root', 'editor']).exists()
+
+User.add_to_class('is_admin_or_editor', is_admin_or_editor)
