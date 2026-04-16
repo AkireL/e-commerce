fe4c233df274b0ca690099c9a8ea54fafed6add3
AkireL <iamdleonor@gmail.com>
2026-04-16 11:49:50 -0600

    added logger

--- orders/api.py
+++ orders/api.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from orders.models import Order
 from orders.serializers import OrderSerializer
-
+from logger.logger import logger
 
 class OrderDetailView(APIView):
     permission_classes = [IsAuthenticated]
@@ -14,9 +14,11 @@ class OrderDetailView(APIView):
         try:
             order = Order.objects.prefetch_related('items').get(pk=pk, user_id=request.user.id)
         except Order.DoesNotExist:
+            logger.warning(f"orders:api OrderDetailView - Order with id {pk} not found for user {request.user.id}.")
             return Response({'error': 'Order not found'}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = OrderSerializer(order)
+        logger.warning(f"orders:api OrderDetailView - Order with id {pk} retrieved successfully for user {request.user.id}.")
         return Response({'order': serializer.data})
 
 
@@ -28,6 +30,7 @@ class OrderMarkPaidView(APIView):
             user_id = request.data.get('user_id')
             order = Order.objects.get(pk=pk, user_id=user_id, is_active=True)
         except Order.DoesNotExist:
+            logger.warning(f"orders:api OrderMarkPaidView -Order with id {pk} not found or already marked as paid for user {user_id}.")
             return Response(
                 {'success': False, 'error': 'Order not found or already paid'},
                 status=status.HTTP_404_NOT_FOUND
@@ -35,5 +38,6 @@ class OrderMarkPaidView(APIView):
 
         order.is_active = False
         order.save(update_fields=['is_active'])
-
+        
+        logger.warning(f"orders:api OrderMarkPaidView - Order with id {pk} marked as paid for user {user_id}.")
         return Response({'success': True})
